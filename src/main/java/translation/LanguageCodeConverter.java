1f8fb32032ae37eb1baf8c8dfac3501c73e8d45f
Park Jihyun <white040707@gmail.com>
2025-09-22 16:10:35 -0400

    Completed task A

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -42,7 +42,17 @@ public class LanguageCodeConverter {
             iterator.next(); // skip the first line
             while (iterator.hasNext()) {
                 String line = iterator.next();
-                // TODO Task A: use line to populate the instance variables
+                if (line == null) continue;
+                if (lines.isEmpty()) continue;
+
+                String [] parts = line.split("[,\\.\\s]");
+                String language = parts[0];
+                String languageCode = parts[1];
+
+                if (languageCode.isEmpty() || language.isEmpty()) continue;
+                languageCodeToLanguage.put(languageCode, language);
+                languageToLanguageCode.put(languageCode, language);
+
             }
 
         } catch (IOException | URISyntaxException ex) {
@@ -56,8 +66,7 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        return code;
+        return languageCodeToLanguage.get(code);
     }
 
     /**
@@ -66,8 +75,7 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        return language;
+        return languageToLanguageCode.get(language);
     }
 
     /**
